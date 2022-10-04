26f6bf145266858ab0ba8aab727f6f1d2b7665eb
Carlos Bello <114359972+cbello6196@users.noreply.github.com>
2022-10-03 20:12:40 -0600

    Clases

---
+++ src/Cubiertos.java
@@ -0,0 +1,29 @@
+
+public class Cubiertos {
+    
+    private String tipo;
+    private int cubiertos;
+
+    public Cubiertos(String tipo, int cubiertos) {
+        this.tipo = tipo;
+        this.cubiertos = cubiertos;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getCubiertos() {
+        return cubiertos;
+    }
+
+    public void setCubiertos(int cubiertos) {
+        this.cubiertos = cubiertos;
+    }
+
+
+}
