b35a0ebb6d40da5bb0d2cc1a7657a8af3691d3d9
Carlos Bello <114359972+cbello6196@users.noreply.github.com>
2022-10-05 15:04:08 -0600

    Cubiertos

--- src/Cubiertos.java
+++ src/Cubiertos.java
@@ -1,5 +1,20 @@
 
 class Cubiertos {
+    private int numCubiertos;
 
-    
+    public Cubiertos(int numCubiertos) {
+        this.numCubiertos = numCubiertos;
+    }
+
+    public int getNumCubiertos() {
+        return numCubiertos;
+    }
+
+    public void setNumCubiertos(int numCubiertos) {
+        this.numCubiertos = numCubiertos;
+    }
+
+    public String toString() {
+        return numCubiertos + " cubiertos ";
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
 
+
+//Autor Carlos Esteban Bello Salinas
+
 public class Main {
     public static void main(String[] args) {
     
