dfcc576d3134fae7d8d775bce2147572beb383e0
Carlos Bello <114359972+cbello6196@users.noreply.github.com>
2022-10-05 15:28:07 -0600

    Main

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,10 @@
 public class Main {
     public static void main(String[] args) {
     
+    Pila pila=new Pila();
+    pila.apilar(new Nodo("Plato",5));
+    pila.apilar(new Nodo("Plato",0));
+    pila.apilar(new Nodo("Vaso",0));
+    pila.imprimir();
     }    
 }    
