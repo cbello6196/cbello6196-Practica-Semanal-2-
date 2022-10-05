cc0b41360c61e5b089ff679ef202fecca170f8f0
Carlos Bello <114359972+cbello6196@users.noreply.github.com>
2022-10-05 15:16:46 -0600

    Pila

--- src/Pila.java
+++ src/Pila.java
@@ -1,5 +1,45 @@
 
 
 public class Pila {
-
+   private Nodo cima;
+   
+   public Pila(){
+       cima=null;
+   }
+   
+   public boolean estaVacia(){
+       return cima==null;
+   }
+   
+   public void apilar(Nodo n){
+       if(estaVacia()){
+           cima=n;
+       }else{
+           n.setSiguiente(cima);
+           cima=n;
+       }
+   }
+   
+   public Nodo desapilar(){
+       if(!estaVacia()){
+           Nodo aux=cima;
+           cima=cima.getSiguiente();
+           aux.setSiguiente(null);
+           return aux;
+       }else{
+           return null;
+       }
+   }
+   
+   public Nodo siguiente(){
+       return cima;
+   }
+   
+   public void imprimir(){
+       Nodo aux=cima;
+       while(aux!=null){
+           System.out.println(aux);
+           aux=aux.getSiguiente();
+       }
+   }
 }
